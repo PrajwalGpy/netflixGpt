eae802e21cb2b18a615705869257a8606ab4a59b
Prajwal Gopal Poojary <prajwalgpy@gmail.com>
2024-08-01 17:05:16 +0530

    day2

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,18 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import Header from "./Header";
 import netflix_Bnner from "../assets/netflix-Login-BG-img.jpg";
+import { checkValiata } from "../utils/Validate";
 
 const Login = () => {
   let [userSignIn, setUserSignIn] = useState(true);
+  const [errorMessage, letErrorMessage] = useState("");
+  const email = useRef(null);
+  const password = useRef(null);
+  const handleSubmit = () => {
+    let message = checkValiata(email.current.value, password.current.value);
+    letErrorMessage(message);
+  };
+
   function toggleSign() {
     setUserSignIn(!userSignIn);
   }
@@ -14,6 +23,7 @@ const Login = () => {
         <img src={netflix_Bnner} alt="netflix_Bnner" />
       </div>
       <form
+        onSubmit={(e) => e.preventDefault()}
         action=""
         className="absolute bg-black bg-opacity-90 text-white w-3/12 my-40 mx-auto left-0 right-0 flex flex-col gap-5 p-10 rounded-lg"
       >
@@ -24,23 +34,26 @@ const Login = () => {
           <input
             type="text"
             placeholder="Full Name"
-            value={""}
             className="bg-slate-900 py-3 px-2 rounded-md"
           />
         )}
         <input
+          ref={email}
           type="text"
           placeholder="Email or mobile number"
-          value={""}
           className="bg-slate-900 py-3 px-2 rounded-md"
         />
         <input
+          ref={password}
           type="text"
           placeholder="Password"
-          value={""}
           className="bg-slate-900 py-3 px-2  rounded-md"
         />
-        <button className="bg-red-700 w-full py-2 rounded-md font-semibold">
+        <div className="font-medium text-lg text-red-700">{errorMessage}</div>
+        <button
+          className="bg-red-700 w-full py-2 rounded-md font-semibold"
+          onClick={handleSubmit}
+        >
           {userSignIn ? "Sign In" : "Sign Up"}
         </button>
         <div className="cursor-pointer" onClick={toggleSign}>
---
+++ src/utils/Validate.jsx
@@ -0,0 +1,10 @@
+export const checkValiata = (email, password) => {
+  const isPasswordValid =
+    /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
+  const isEmailValidation = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+
+  if (!isEmailValidation) return "Email is not  valid";
+  if (!isPasswordValid) return "PassWord is not valid";
+
+  return null;
+};
